f1b3941793817c3645711564ca82774417c24800
Jongyeol <45934107+Jongye0l@users.noreply.github.com>
2024-03-16 00:10:51 +0900

    Initial commit

---
+++ BGAMod/PlayPatch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using ADOFAI;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BGAMod {
+    [HarmonyPatch]
+    public class PlayPatch {
+        [HarmonyPatch(typeof(scnEditor), "Play")]
+        [HarmonyPostfix]
+        public static void Hide() {
+            foreach(scrFloor floor in scnEditor.instance.floors) floor.startScale = floor.transform.localScale = Vector3.zero;
+            foreach(scrPlanet planet in scrController.instance.allPlanets) {
+                for(int i = 0; i < planet.transform.childCount; i++) 
+                    planet.transform.GetChild(i).gameObject.SetActive(false);
+                planet.gameObject.GetComponent<PlanetRenderer>().visible = false;
+            }
+            scrConductor.instance.hitSoundVolume = 0;
+        }
+        
+        [HarmonyPatch(typeof(scnGame), "ApplyEvent")]
+        [HarmonyPostfix]
+        public static void RemoveMultiPlanet(LevelEvent evnt, List<scrFloor> floors, int? customFloorID, ffxPlusBase __result) {
+            int num1 = customFloorID ?? evnt.floor;
+            scrFloor floor = floors[num1];
+            if(__result is ffxMoveFloorPlus ffxMoveFloorPlus) ffxMoveFloorPlus.scaleUsed = false;
+            if(evnt.eventType == LevelEventType.SetHitsound) floor.setHitsound = null;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# BGAMod
+![C#](https://img.shields.io/badge/Lang-Csharp-c9c8e4.svg?&logo=csharp)
+![Rider](https://img.shields.io/badge/IDE-Rider-c9c8e4.svg?&logo=rider)
+![Download](https://img.shields.io/github/downloads/Jongye0l/BGAMod/PesudoMaker.zip)
+---
+### 소개 Information
+* 얼불춤에 타일과 공을 제거해줍니다
+---
+### 사용 방법 How to use
+* [이 링크](https://github.com/Jongye0l/BGAMod/releases/latest) 에서 다운로드 후 모드매니저를 통해 적용시키면 됩니다.
+---
+### 개발 환경 Development environment
+* Windows 11
+* Rider 2023.3.4
+* .NET Framework 4.8
+* A Dance of Fire and Ice 2.7.0(r121, 5032090)
+---
+### 테스트 환경 Test environment
+* Windows 11
+* A Dance of Fire and Ice 2.7.0(r121, 5032090)
+---
+# [Join My Discord!](https://discord.jongyeol.kr)
